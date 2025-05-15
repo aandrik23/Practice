4a0bdc170d675c7154bd4d3366fc062e0cecd6fe
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-05-15 23:26:04 +0300

    fishandchips

---
+++ 20/fishandchips/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	// add "piscine"
+)
+
+func main() {
+	fmt.Println(FishAndChips(4))
+	fmt.Println(FishAndChips(9))
+	fmt.Println(FishAndChips(6))
+	// all of them are fmt.Println(piscine.FishAndChips(4 or 6 or 9 e.tc))
+}
